93248114e37f77ee2db3a46c605ec69b8aff372d
rkstore-admin <ravedits9@gmail.com>
2026-01-09 17:05:05 +0530

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer style={{ marginTop: "120px", textAlign: "center", opacity: 0.6 }}>
+      © {new Date().getFullYear()} Kinetic’s Home
+    </footer>
+  );
+}
