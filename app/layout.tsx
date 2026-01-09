c17cbb50ac49b153dff381f5061245195da25840
rkstore-admin <ravedits9@gmail.com>
2026-01-09 17:02:17 +0530

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import "./globals.css";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main className="min-h-screen px-4">{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
